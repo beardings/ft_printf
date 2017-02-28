2859ec636582d3b792e512677b743007b6c8e91f
mponomar <mponomar@student.unit.ua>
2017-02-28 17:02:40 +0200

    fonderfull "S"

--- main.c
+++ main.c
@@ -14,10 +14,10 @@ int main(void)
     //ft_printf("\n(FT_): %04.2s %-15.0s|", str, res);
      setlocale(LC_ALL, "");
     printf("\n");
-    printf("|len: %i", ft_printf("my: %4.S", L"жM-M-^QжM-^XѓдЄM-еM-^O™зM-^LЂгM-M-^B"));
+    printf("|len: %i", ft_printf("my: %S", L"@@"));
     //ft_printf("%S", L"米");
     printf("\n");
-    printf("|len: %i", printf("or: %4.S", L"жM-M-^QжM-^XѓдЄM-еM-^O™зM-^LЂгM-M-^B"));
+    printf("|len: %i", printf("or: %S", L"@@"));
     //printf("%S", L"米");
     //ft_printf("%s%s%s%s%s", "this", "is", "a", "multi", "string");
     //printf("Hallo Nikolas%%% -20.47dhellogame", 12);
